223dfdad522db808e4c2f0e6213eb2db6c2a7627
salwazpw <salwapw04@gmail.com>
2022-10-28 15:32:59 +0700

    Update Footer

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,4 +1,5 @@
 <footer class="footer text-center text-muted">
-    Copyright by Nugraha Trans. Designed and Developed by <a
-        href="#">Sanum</a>.
+    Perumahan Joyogrand B1/10, Merjosari Kec.Lowokwaru, Kota Malang, Jawa Timur 65144 <br>
+    Nugrahacorp.inc@gmail.com <br>
+    081234141643
   </footer>
