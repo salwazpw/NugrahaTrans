9bee29dee4db7995837873b8f7f97946c28c51c5
salwazpw <salwapw04@gmail.com>
2022-10-29 11:58:53 +0700

    Web Katalog Kendaraan

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\KatalogController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,6 +14,4 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('layouts.coba');
-});
+Route::resource('katalog',KatalogController::class);
