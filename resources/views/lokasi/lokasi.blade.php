cd833491467814e02c9a25c7cb64aab4ee77c798
salwazpw <salwapw04@gmail.com>
2022-11-04 20:54:17 +0700

    Pembuatan Halaman Lokasi dan Petunjuk Arah

---
+++ resources/views/lokasi/lokasi.blade.php
@@ -0,0 +1,8 @@
+@extends('layouts.main')
+@section('title')
+Lokasi | NugrahaTrans
+@endsection
+@section('content')
+<h1 style="font-family:'Times New Roman', Times, serif">Peta Lokasi dan Petunjuk arah NugrahaTrans</h1>
+<iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d15806.154536874994!2d112.5939425!3d-7.9431561!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x32bc82fc7bcd2c87!2sNugrahaCorp.Inc!5e0!3m2!1sen!2sid!4v1667567501119!5m2!1sen!2sid" width="800" height="450" style="border:10px dashed rgb(90, 13, 231); margin-left: 120px" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+@endsection
