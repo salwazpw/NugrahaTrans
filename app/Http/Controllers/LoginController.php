b84487fea4dcbd3fbdb875397bee816e52d56717
salwazpw <salwapw04@gmail.com>
2022-11-07 19:17:43 +0700

    Pembuatan Login

---
+++ app/Http/Controllers/LoginController.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Str;
+
+class LoginController extends Controller
+{
+    public function login()
+    {
+        return view('login.login');
+    }
+
+    public function postLogin(Request $request)
+    {
+        if (Auth::attempt($request->only('username', 'password'))) {
+            return redirect('/katalog');
+        }
+        return redirect('/login');
+    }
+
+    public function logout(Request $request)
+    {
+        Auth::logout();
+        return redirect('/login');
+    }
+
+    public function registrasi()
+    {
+        return view('login.registrasi');
+    }
+
+    public function postRegistrasi(Request $request)
+    {
+        User::create([
+            'name' => $request->name,
+            'level' => 'user',
+            'profile' => null,
+            'username' => $request->username,
+            'email' => $request->email,
+            'password' => bcrypt($request->password),
+            'remember_token' => Str::random(60),
+        ]);
+
+        return redirect('/katalog');
+    }
+}
