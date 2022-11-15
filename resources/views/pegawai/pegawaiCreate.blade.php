df3262d6348015a3eefaf5b24cc76f5afc7774c2
salwazpw <salwapw04@gmail.com>
2022-11-15 17:52:53 +0700

    Revisi Pegawai

--- resources/views/pegawai/pegawaiCreate.blade.php
+++ resources/views/pegawai/pegawaiCreate.blade.php
@@ -25,7 +25,7 @@
                     <div class="col-md-12 col-xs-12">
                         <div class="form-group">
                             <label for="exampleInputEmail1">NIP</label>
-                            <input type="text" class="form-control" aria-describedby="emailHelp" placeholder="NIP" name="id" required readonly value="{{ auth()->user()->nik }}">
+                            <input type="text" class="form-control" aria-describedby="emailHelp" placeholder="NIP" name="id" required>
                         </div>
                     </div>
                     <div class="col-md-12 col-xs-12">
